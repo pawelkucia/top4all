591803bf52535b7e49bcce52d9b6bd926ac3a78d
pawelkucia <pawel.kucia@gmail.com>
2025-02-22 15:49:18 +0100

    Logo fixed.

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -8,11 +8,11 @@
     <div class="container py-2">
       <nav class="flex items-center justify-between">
         <NuxtLink to="/" class="text-primary flex items-center">
-          <NuxtImg :src="logoSrc" alt="Top4All" class="h-[72px] transform origin-left" height="72" width="193" />
+          <img :src="logoSrc" alt="Top4All" class="h-[72px] transform origin-left" height="72" width="193">
         </NuxtLink>
 
         <!-- Desktop Navigation -->
-        <div class="hidden md:flex items-center space-x-6">
+        <div class=" hidden md:flex items-center space-x-6">
           <NavLink to="/seo">SEO</NavLink>
           <NavLink to="/google-ads">Google Ads</NavLink>
           <ServicesDropdown />
