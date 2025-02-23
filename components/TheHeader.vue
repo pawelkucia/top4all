81486a376ac07a9af7c3c7c3f0392426af709a0b
pawelkucia <pawel.kucia@gmail.com>
2025-02-23 22:48:00 +0100

    Header - logo changed after scroll.

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,14 +1,14 @@
 <template>
-  <header class="fixed top-[5px] left-[5px] right-[5px] z-50 " >
-    <div class="container py-2 transition-colors duration-300" :class="[
-    isScrolled || isMobileMenuOpen
-      ? 'bg-white/50 backdrop-blur-md dark:bg-dark-100/40 shadow-lg'
-      : 'bg-transparent',
-    isMobileMenuOpen ? 'rounded-2xl' : 'rounded-full'
+  <header class="fixed top-[5px] left-[5px] right-[5px] z-50 ">
+    <div class="container py-2 px-2 transition-colors duration-300" :class="[
+      isScrolled || isMobileMenuOpen
+        ? 'bg-white/60 backdrop-blur-md dark:bg-dark-100/40 shadow-lg'
+        : 'bg-transparent',
+      isMobileMenuOpen ? 'rounded-2xl' : 'rounded-full'
     ]">
       <nav class="flex items-center justify-between">
         <NuxtLink to="/" class="text-primary flex items-center">
-          <img :src="logoSrc" alt="Top4All" class="h-[72px] transform origin-left" height="72" width="193">
+          <img :src="logoSrc" alt="Top4All" class="transition-all duration-300" :class="[isScrolled && !isMobileMenuOpen? 'h-[48px] rounded-full' : 'h-[72px]']" :height="isScrolled && !isMobileMenuOpen ? 48 : 72" :width="isScrolled && !isMobileMenuOpen ? 48 : 193">          
         </NuxtLink>
 
         <!-- Desktop Navigation -->
@@ -23,10 +23,10 @@
         <!-- Mobile Menu Button -->
         <div class="md:hidden flex items-center space-x-4">
           <ThemeToggle />
-          <button class="bhover:text-primary dark:hover:text-primary transition-colors duration-200" :class="[
+          <button class="p-2 hover:text-primary dark:hover:text-primary transition-colors duration-200" :class="[
             isScrolled || isMobileMenuOpen ? 'text-gray-700 dark:text-gray-300' : 'text-white'
           ]" @click="toggleMobileMenu" title="Toggle Menu">
-            <i class="fas fa-bars text-xl"></i>
+            <i class="fas text-xl" :class="[isMobileMenuOpen ? 'fa-close' : 'fa-bars']"></i>
           </button>
         </div>
       </nav>
@@ -54,12 +54,17 @@ const isServicesOpen = ref(false);
 const isScrolled = ref(false);
 
 const logoSrc = computed(() => {
-  if (themeStore.isDark) {
-    return '/images/logo-white-orange.png';
+  let logoUrl = '/images/logo-white-orange.png';
+
+  if (isMobileMenuOpen.value) {
+    logoUrl = themeStore.isDark ? '/images/logo-white-orange.png' : '/images/logo.png';
+  } else if (isScrolled.value) {
+    logoUrl = '/images/logo-short.jpg';
+  } else {
+    logoUrl = '/images/logo-white-orange.png';
   }
-  return (isScrolled.value || isMobileMenuOpen.value)
-    ? '/images/logo.png'
-    : '/images/logo-white-orange.png';
+
+  return logoUrl;
 });
 
 const toggleMobileMenu = () => {
